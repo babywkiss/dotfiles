ca75e36be5acf666ca3a7bd99b39fcc7a0336bf8
nicejji <nicejji@proton.me>
2024-02-19 04:18:39 +0300

    refactoring

--- fish/config.fish
+++ fish/config.fish
@@ -1,10 +1,6 @@
 fish_vi_key_bindings
 fish_config theme choose "Rosé Pine"
 
-set -g fish_greeting
-set -g EDITOR nvim
-set -g VISUAL nvim
-
 alias ls="eza --icons"
 alias lt="eza --icons -T"
 alias la="eza --icons -a"
